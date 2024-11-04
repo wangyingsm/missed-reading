7c3d36c412e7b1b4413da312d70bf881a1f983f9
Ying Wang <yingge_gzb@qq.com>
2024-11-05 00:01:36 +0800

    sicp chapter 2 part 2 sources

--- chap2/cpx-rs/target/debug/deps/cpx_rs-38026a883e50cf29.d
+++
@@ -1,9 +0,0 @@
-/home/wangy/workspace/sicp/chap2/cpx-rs/target/debug/deps/libcpx_rs-38026a883e50cf29.rmeta: src/lib.rs Cargo.toml
-
-/home/wangy/workspace/sicp/chap2/cpx-rs/target/debug/deps/cpx_rs-38026a883e50cf29.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
